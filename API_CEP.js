0a772c7f823a056a2d22c556e7ad52e3976943bd
Felipe de Oliveira Marchioli <44444830+FelipeMarchioli@users.noreply.github.com>
2019-02-15 16:21:53 -0200

    Update API_CEP.js

--- API_CEP.js
+++ API_CEP.js
@@ -1,21 +1,26 @@
+//botão limpar
 $('input.limpar').on('click', function() {
 
   
   $('div.form-group').find('input').val('');
 
 });
+//função que carrega ao iniciar a página
 $(document).ready (function() {
   $('#cep').blur (function() {
+    //variável recebendo cep caso ele não seja vazio
     var cep = $('#cep').val() || ''
+    //caso não exista o cep não retorna nada
     if(!cep) {
       return
     }
     var url = 'https://viacep.com.br/ws/' + cep + '/json'
+    //carregando dados no formato JSON
     $.getJSON(url, function(data) {
       if("error" in data) {
         return
       }
-      
+      //enviando os dados para carregar ao usuario
       $('#logradouro').val(data.logradouro)
       $('#bairro').val(data.bairro)
       $('#localidade').val(data.localidade)
